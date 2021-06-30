0b596e40da2a7427e45e307c9e946bc50f2d66d9
Troy Ault <aultt@hotmail.com>
2021-06-30 09:26:39 -0400

    adding in ansible install to reference

---
+++ AppZone/Hub_Spoke/Single_Region/Oracle_Single/ansible/oracle-setup-ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update all packages that have available updates.
+sudo yum update -y
+
+# Install Python 3 and pip.
+sudo yum install -y python3-pip
+
+# Upgrade pip3.
+sudo pip3 install --upgrade pip
+
+# Install Ansible.
+pip3 install ansible[azure]
+
+# Install Ansible modules and plugins for interacting with Azure.
+ansible-galaxy collection install azure.azcollection
+
+# Install required modules for Ansible on Azure
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+
+# Install Ansible modules
+sudo pip3 install -r requirements-azure.txt
