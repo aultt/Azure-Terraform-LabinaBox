a5fe8abd6b4c19adf2599f6a08e60bfbdbd10680
Troy Ault <aultt@hotmail.com>
2021-07-01 17:36:14 -0400

    updating to build ansible deploy

--- AppZone/Hub_Spoke/Single_Region/Oracle_Single/bash/oracle-setup-ansible.sh
+++ AppZone/Hub_Spoke/Single_Region/Oracle_Single/bash/oracle-setup-ansible.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+while getopts g:o:r:w:u:y:s:m:d: option
+    do case "${option}"
+        in
+    g) GRIDPASS=${OPTARG};;
+    o) ORACLEPASS=${OPTARG};;
+    r) ROOTPASS=${OPTARG};;
+    w) SWAPSIZE=${OPTARG};;
+    u) STORAGEURL=${OPTARG};;
+    y) SYSPASS=${OPTARG};;
+    s) SYSTEMPASS=${OPTARG};;
+    m) MONITORPASS=${OPTARG};;
+    d) DBNAME=${OPTARG};;
+        esac
+        done
 # Update all packages that have available updates.
 sudo yum update -y
 
@@ -20,3 +34,11 @@ wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirement
 
 # Install Ansible modules
 sudo pip3 install -r requirements-azure.txt
+
+# Build File to Kick of Ansible Playbook
+echo "ansible-playbook Configure-ASM-server.yml -e gridpass='$GRIDPASS' -e oraclepass='$ORACLEPASS' -e rootpass='$ROOTPASS' -e swapsize='$SWAPSIZE' -e gridurl='$STORAGEURL' -e syspass='$SYSPASS' -e systempass='$SYSTEMPASS' -e monitorpass='$MONITORPASS' -e dbname='$DBNAME'" >>/home/azureadmin/deployoracle.sh
+chown azureadmin /home/azureadmin/deployoracle.sh
+chmod 700 /home/azureadmin/deployoracle.sh
+cp /var/lib/waagent/custom-script/download/0/Configure-ASM-server.yml /home/azureadmin
+chown azureadmin /home/azureadmin/Configure-ASM-server.yml
+chmod 700 /home/azureadmin/Configure-ASM-server.yml
